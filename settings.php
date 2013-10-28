0aec2d303038692b8645245c10079d6610f74dff
nebriv <nebriv@gmail.com>
2013-10-27 20:01:27 -0400

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,38 @@
+<?php
+
+//Configuration Options
+
+//Full name is hack job to check that no one else can load their LinkedIn profile into your website
+$fullnamelinkedin = "YOUR LINKED IN FULL NAME";
+$password = "INSERT RANDOME PASSWORD";
+
+//Website URL
+$baseurl = "http://example.com";
+
+
+//Information to pull in from LinkedIn
+$skills = True;
+$positions = True;
+$courses = True;
+$summary = True;
+$organizations = False;
+$interests = False;
+$certifications = True;
+$picture = True;
+$full_name = True;
+$email = True;
+$phone_numbers = False;
+
+
+//xmlfile to store resume information
+$xmlfile = "profile.xml";
+
+//----------------------
+//LinkedIn API Settings
+//-----------------------
+//API Keys
+define("CONSUMER_KEY", "KEY");
+define("CONSUMER_SECRET", "SECRET");
+
+//Permission Scope Requested
+$scope = "r_fullprofile+r_basicprofile+r_network+r_contactinfo";
